0b52ca5a45df48c856fdf9a6eac9bd85fc803a22
mnogotveth <49138730+mnogotveth@users.noreply.github.com>
2020-04-10 11:06:19 +0700

    Update array-holder.unit.test.js

--- app/modules/array-holder.unit.test.js
+++ app/modules/array-holder.unit.test.js
@@ -1,75 +1,72 @@
 import ArrayHolder from "./array-holder";
 
 const array = [1, 2, 3];
+
 function getInitialData() {
   const arrayHolder = new ArrayHolder();
   arrayHolder.set(array);
   return arrayHolder;
 }
-
+//classwork
 test('get unexsisting element', () => {
   const arrayHolder = new ArrayHolder();
-  expect(() => {arrayHolder.getElementByIndex(0)}).toThrow();
+  expect(() => {
+    arrayHolder.getElementByIndex(0)
+  }).toThrow();
 });
 
 test('get array sum', () => {
   const arrayHolder = new ArrayHolder();
-  expect(() => {arrayHolder.sum()}).toThrow();
+  expect(() => {
+    arrayHolder.sum()
+  }).toThrow();
   arrayHolder.set(array);
   expect(arrayHolder.sum()).toBe(6);
 });
 
-test ('unit test 1', () => {
+//homework
+test('expect 0 0 0 0', () => {
   const arrayHolder = new ArrayHolder();
   expect(arrayHolder.average()).toBe(0);
   expect(arrayHolder.geometricMean()).toBe(0);
-  expect(() => {arrayHolder.sum()}).toThrow();
   expect(arrayHolder.min()).toBe(0);
   expect(arrayHolder.max()).toBe(0);
 });
 
-
-const arr = [1];
-test ('unit test 2', () => {
+test('set 1, expext 1 1 1 1', () => {
   const arrayHolder = new ArrayHolder();
-  arrayHolder.set(arr);
+  arrayHolder.set(['1']);
   expect(arrayHolder.average()).toBe(1);
   expect(arrayHolder.geometricMean()).toBe(1);
-  expect(arrayHolder.sum()).toBe(1);
   expect(arrayHolder.min()).toBe(1);
   expect(arrayHolder.max()).toBe(1);
 });
 
-const arr = [1,2,3];
-test('unit test 3', () => {
+test('set 1 2 3', () => {
   const arrayHolder = new ArrayHolder();
-  arrayHolder.set(arr);
+  arrayHolder.set(array);
   expect(arrayHolder.average()).toBe(2);
   expect(arrayHolder.geometricMean()).toBe(Math.pow(6, 1/3));
-  expect(arrayHolder.sum()).toBe(6);
   expect(arrayHolder.min()).toBe(1);
   expect(arrayHolder.max()).toBe(3);
 });
-  const arr = [1,2,3];
-  test('unit test 4', () => {
-    const arrayHolder = new ArrayHolder();
-    arrayHolder.set(arr);
-    arrayHolder.removeElementByIndex(2);
-    expect(arrayHolder.average()).toBe(1.5);
-    expect(arrayHolder.geometricMean()).toBe(Math.pow(3, 1/2));
-    expect(arrayHolder.sum()).toBe(3);
-    expect(arrayHolder.min()).toBe(1);
-    expect(arrayHolder.max()).toBe(2);
-  });
 
-const arr = [1,2,3];
-test('unit test 5', () => {
+test('delete last value', () => {
   const arrayHolder = new ArrayHolder();
-  arrayHolder.set(arr);
+  arrayHolder.set(array);
+  arrayHolder.removeElementByIndex(arrayHolder.arrayStore.length - 1);
+  expect(arrayHolder.average()).toBe(1.5);
+  expect(arrayHolder.geometricMean()).toBe(Math.pow(3, 1/2));
+  expect(arrayHolder.min()).toBe(1);
+  expect(arrayHolder.max()).toBe(2);
+});
+
+test('delete all values', () => {
+  const arrayHolder = new ArrayHolder();
+  arrayHolder.set(array);
   arrayHolder.clear();
   expect(arrayHolder.average()).toBe(0);
   expect(arrayHolder.geometricMean()).toBe(0);
-  expect(() => {arrayHolder.sum()}).toThrow();
   expect(arrayHolder.min()).toBe(0);
   expect(arrayHolder.max()).toBe(0);
 });
